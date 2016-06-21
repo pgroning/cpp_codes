00c20cdc55aee89d4598f679543a48743147b16c
Per Gröningsson <per.groningsson@vattenfall.com>
2016-06-21 11:08:29 +0200

    added option to list more than one file in lsp.cpp

--- lsp.cpp
+++ lsp.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
@@ -20,12 +21,24 @@ bool fileExists(const string& filename)
     return false;
 }
 
-
+void help()
+{
+  cout << "Usage: lsp [FILE1] [FILE2]...\n";
+  cout << "List FILEs with absolute paths.\n\n";
+  cout << "Options:\n";
+  cout << " -h   : show this help message and exit\n";
+}
 
 int main(int argc, char* argv[])
 {
   string inarg;
   
+  if (!strcmp(argv[1],"-h")) { // call help function
+    help();
+    exit(0);
+  }
+
+  /*  
   //int n = sizeof(argv)/sizeof(*argv);
   if (argc > 2) { // Too many input argument are given
     cout << "Error: Too many input arguments\n";
@@ -37,24 +50,31 @@ int main(int argc, char* argv[])
   else {
     inarg = "";
   }
+  */
 
-  if(!inarg.empty()) { // Check if file exists
-    bool exist = fileExists(inarg);
-    if (!exist) {
-      string errmsg = "lsp: cannot access '"+inarg+"'";
-      perror(errmsg.c_str());
+  for (int i = 1; i < argc; i++) {
+    
+    inarg = argv[i];
+    
+    if(!inarg.empty()) { // Check if file exists
+      bool exist = fileExists(inarg);
+      if (!exist) {
+        string errmsg = "lsp: cannot access '"+inarg+"'";
+        perror(errmsg.c_str());
+        exit(EXIT_FAILURE);
+      }
+    }
+    
+    char buffer[PATH_MAX+1];
+    char *result = realpath("./", buffer);
+    if (result) {
+      cout << buffer;
+      cout << "/" << inarg << endl;
+    }
+    else {
       exit(EXIT_FAILURE);
     }
-  }
-  
-  char buffer[PATH_MAX+1];
-  char *result = realpath("./", buffer);
-  if (result) {
-    cout << buffer;
-    cout << "/" << inarg << endl;
-  }
-  else {
-    exit(EXIT_FAILURE);
+    
   }
 
   return 0;
