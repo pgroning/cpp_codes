9abd91b21c7c14df77cadb4462c00f9ea6be0932
Per Gröningsson <per.groningsson@gmail.com>
2016-10-24 23:23:43 +0200

    created list object

--- filedups/filedups.cpp
+++ filedups/filedups.cpp
@@ -1,6 +1,7 @@
 /* Search file tree recursively to find duplicate files */
 
 #include <iostream>
+#include <list>
 
 using namespace std;
 
@@ -8,5 +9,11 @@ int main() {
 
   cout << "Find duplicte files" << endl;
 
-
+  list<string> filelist;  // list of strings
+  list<string>::iterator ptr;
+  filelist.push_front("Hello");
+  filelist.push_back("World");
+  ptr = filelist.begin();
+  cout << *ptr++ << endl;
+  cout << *ptr << endl;
 }
