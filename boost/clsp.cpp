93b4d78a884f24475919498bf8e7624ea42f671c
Per Gröningsson <per.groningsson@gmail.com>
2016-11-22 23:46:16 +0100

    added boost/clsp

---
+++ boost/clsp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+using namespace boost::filesystem;
+
+int main() {
+  
+  path path("../a.out");
+  
+  cout << path.string() << endl;
+  cout << path.root_name().c_str() << endl;
+  cout << path.root_directory().string() << endl;
+  cout << path.relative_path().string() << endl;
+  cout << path.parent_path().string() << endl;
+  cout << path.filename().string() << endl;
+  cout << path.stem().string() << endl;
+  cout << path.extension().string() << endl;
+
+  cout << "--------" << endl;
+  string abspath = absolute(path).string();
+  cout << abspath << endl;
+  cout << canonical(path) << endl;
+  cout << path.is_relative() << endl;
+  cout << path.is_absolute() << endl;
+  
+  return 0;
+}
