1c588317f21d67df0264acb514a8fe9e339fa3aa
Per Gröningsson <per.groningsson@vattenfall.com>
2016-11-29 11:26:19 +0100

    changed member path name to p. updated the constructor accordingly.

--- boost/clsp.cpp
+++ boost/clsp.cpp
@@ -9,8 +9,7 @@ void help();
 
 class Dir {
 private:
-  path this_path;
-
+  path p;
   
 public:
   Dir(path p);  // constructor
@@ -22,17 +21,17 @@ public:
 
 int main(int argc, char* argv[]) {
 
-  string inarg;
-  
   /* Get input parameters */
   if (argc > 1 && !strcmp(argv[1], "-h")) {
     help(); // call help function
     exit(0);
   }
-  
-  for (int i = 1; i < argc; i++) {
 
-    path p(argc>1? argv[i] : ".");
+  int n = argc > 1 ? argc : 2;
+
+  for (int i = 1; i < n; i++) {
+
+    path p(argc > 1 ? argv[i] : ".");
     Dir dir(p);
     
     if (dir.is_absolute()) {
@@ -76,21 +75,20 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
-Dir::Dir(path p) {
-  this_path = p;
-}
+Dir::Dir(path p) : p(p) {} // sets the member p equal to variable p
+
 
 path Dir::get_path() {
-  return this_path;
+  return p;
 }
 
 int Dir::is_absolute() {
-  return this_path.is_absolute();
+  return p.is_absolute();
 }
 
 void Dir::list_all() {
 
-  for (auto& f : boost::make_iterator_range(directory_iterator(this_path), {})) {
+  for (auto& f : boost::make_iterator_range(directory_iterator(p), {})) {
     cout << canonical(f.path()).string() << endl;
   } 
   
@@ -101,34 +99,33 @@ path Dir::rel_path() {
   
   path wd("."); // current working directory
   path pwd(absolute(wd));
-  if (this_path.root_directory() != pwd.root_directory()) {
-    return this_path;
-    //cout << this_path.string() << endl; // return p if root paths are different
+  if (p.root_directory() != pwd.root_directory()) {
+    return p;
+    //cout << p.string() << endl; // return p if root paths are different
   }
   else {
     path::const_iterator fromIter = pwd.begin();
-    path::const_iterator toIter = this_path.begin();
+    path::const_iterator toIter = p.begin();
     
     // Loop through both paths as long as they are the same
-    while (fromIter != this_path.end() && toIter != pwd.end() &&	\
+    while (fromIter != p.end() && toIter != pwd.end() &&	\
 	   (*toIter) == (*fromIter)) {
       ++toIter;
       ++fromIter;
     }
     
     path relPath;
-    //finalPath = ".";
     ++fromIter;
     while (fromIter != pwd.end()) {
       relPath /= "..";
       ++fromIter;
     }
-    while (toIter != this_path.end()) {
+    while (toIter != p.end()) {
       relPath /= *toIter;
       ++toIter;
     }
     
-    //cout << finalPath.string() << endl;
+    //cout << relPath.string() << endl;
     return relPath;
     
   }
@@ -140,7 +137,7 @@ path Dir::rel_path() {
 void help() {
   cout << "clsp:\n";
   cout << "Usage: clsp [Options]... [FILE]...\n";
-  cout << "List FILEs (the current directory by default) with absolute or relative paths.\nThe program is using the boost C++ library.\n\n";
+  cout << "List FILEs (the current directory by default) with absolute or relative paths.\nThe program is making use of the Boost C++ library.\n\n";
   
   cout << "Options:\n";
   cout << "-h     : Show this help message and exit\n";
