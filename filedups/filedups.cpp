4452ec29f6ac90c8f8fe044a1c7ca0785532ec09
Per Gröningsson <per.groningsson@gmail.com>
2016-12-03 15:31:14 +0100

    filedups: store all files in list + created Makefile

--- filedups/filedups.cpp
+++ filedups/filedups.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 #include <list>
+#include "boost/filesystem.hpp"
 
 using namespace std;
+using namespace boost::filesystem;
 
 int main() {
 
@@ -11,9 +13,27 @@ int main() {
 
   list<string> filelist;  // list of strings
   list<string>::iterator ptr;
+
+  // List all files recursivley and store in list
+  for (recursive_directory_iterator end, dir("./"); dir != end; ++dir ) {
+    //cout << dir->path().string() << endl;  // full path
+    //std::cout << dir->path().filename() << "\n"; // just last bit
+    filelist.push_back(dir->path().string()); // Store file path in list
+  }
+  filelist.sort();
+  
+  //ptr = filelist.begin();
+  //for (int i = 0; i < filelist.size(); i++)
+  //  cout << *ptr++ << endl;
+
+  for (ptr = filelist.begin(); ptr != filelist.end(); ptr++)
+    cout << *ptr << endl;
+  
+  /*
   filelist.push_front("Hello");
   filelist.push_back("World");
   ptr = filelist.begin();
   cout << *ptr++ << endl;
   cout << *ptr << endl;
+  */
 }
